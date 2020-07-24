a8ee3f155ef91c0f4520aee0b6faaea1cf035acf
Oliva Basu <olivabasu@gmail.com>
2020-07-24 21:17:12 +0530

    javascript

---
+++ scripts.js
@@ -0,0 +1,45 @@
+function validate(){
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(name.length==0){
+      text = "Name is mandatory field.";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(name.length == 1){
+      text = "Please enter a valid name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+      text = "Please Enter valid Phone Number";
+      error_message.innerHTML = text;
+      return false;
+    }
+
+    if(email.length==0){
+        text = "Email-ID is mandatory field.";
+        error_message.innerHTML = text;
+        return false;
+      }
+
+    if(email.indexOf("@") == -1 || email.length > 0 && email.length < 6){
+      text = "Please Enter valid Email-ID";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length==0){
+      text = "Please write your query.";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
